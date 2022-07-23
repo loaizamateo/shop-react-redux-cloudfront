64f44040451fd4af703564059ab8d9abdd9532da
loaizamateo <mateo_loaiza@epam.com>
2022-07-23 15:23:55 -0500

    - Get product by id was implemented in the task-3 - Create product implemented.

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -109,7 +109,7 @@ export default function PageProductForm() {
   const onSubmit = (values: FormikValues) => {
     const formattedValues = ProductSchema.cast(values);
     const productToSave = id ? {...ProductSchema.cast(formattedValues), id} : formattedValues;
-    axios.put(`${API_PATHS.product}/products`, productToSave)
+    axios.post(`${API_PATHS.product}/products`, productToSave)
       .then(() => history.push('/admin/products'));
   };
 
