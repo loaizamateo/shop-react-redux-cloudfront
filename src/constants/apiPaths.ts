f75e12af1682584f5d119a3cc2a696bb0afd7962
loaizamateo <mateo_loaiza@epam.com>
2022-08-01 02:05:29 -0500

    - import path added

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: `https://${process.env.REACT_APP_URL_PRODUCTS_GATEWAY}.execute-api.us-east-1.amazonaws.com/dev`,
   order: 'https://.execute-api.us-east-1.amazonaws.com/dev',
-  import: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+  import: `https://${process.env.REACT_APP_URL_IMPORT_GATEWAY}.execute-api.us-east-1.amazonaws.com/dev`,
   cart: 'https://.execute-api.us-east-1.amazonaws.com/dev',
 };
 
